645fde59be15578b10c34dae10fb14a6a96870c9
ArnaudTeinturier <arnaud.teinturier@polytechnique.edu>
2024-11-14 10:08:56 +0100

    integration dataCleaning in results

--- src/data/dataLoader.py
+++ src/data/dataLoader.py
@@ -3,7 +3,7 @@ import pandas as pd
 # Function to load dataframes and preprocess the data
 def loadDataframe(name=('movies','characters','clusters','summaries')):
     if name == 'movies':
-        df = pd.read_csv('../../data/cleanData/movies_cleaned.csv')
+        df = pd.read_csv('data/cleanData/movies_cleaned.csv')
         df['release_date'] = pd.to_datetime(df['release_date'])
         
         columns_to_convert = ['genres','languages','countries']
@@ -12,7 +12,7 @@ def loadDataframe(name=('movies','characters','clusters','summaries')):
         return df
     
     elif name == 'characters':
-        df = pd.read_csv('../../data/cleanData/characters_cleaned.csv')
+        df = pd.read_csv('data/cleanData/characters_cleaned.csv')
         
         df['birth_date'] = pd.to_datetime(df['birth_date'])
         df['release_date'] = pd.to_datetime(df['release_date'])
@@ -20,11 +20,11 @@ def loadDataframe(name=('movies','characters','clusters','summaries')):
         return df
         
     elif name == 'clusters':
-        df = pd.read_csv('../../data/cleanData/character_clusters_cleaned.csv')
+        df = pd.read_csv('data/cleanData/character_clusters_cleaned.csv')
         return df
     
     elif name == 'summaries':
-        df = pd.read_csv('../../data/cleanData/summaries_cleaned.csv')
+        df = pd.read_csv('data/cleanData/summaries_cleaned.csv')
         return df
     
     else:
